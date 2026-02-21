1c596f39542209354de2f690dd86c2cd60ee2499
blacksfritching <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>
2026-02-21 02:47:07 -0300

    docs: update server.js to load .env in development and test environments and test connection to Vercel

--- src/server.js
+++ src/server.js
@@ -4,18 +4,15 @@ import { fileURLToPath } from 'node:url';
 import app from './app/app.js';
 import MongoDBConnection from './connections/mongodb.connection.js';
 
-if (process.env.NODE_ENV !== 'production') {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  dotenv.config({ path: path.join(__dirname, '.env') });
-}
-
 const startServer = async () => {
   try {
     await MongoDBConnection.getConnection();
     console.log("Conectado ao MongoDB com sucesso.");
 
     if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      dotenv.config({ path: path.join(__dirname, '.env') });
       const PORT = process.env.PORT || 3000;
       app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
