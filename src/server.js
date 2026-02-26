cb1d891246cffc1bd11edb045e6aa4739da58489
blacksfritching <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>
2026-02-25 22:04:34 -0300

    docs: change in server.js for Vercel to work.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ import app from './app/app.js';
 import MongoDBConnection from './connections/mongodb.connection.js';
 import logger from './config/logger.config.js';
 
+logger.info(
+  {
+    event: 'server_started',
+    env: process.env.NODE_ENV,
+    version: process.env.npm_package_version,
+  },
+  '[SERVER] Running and healthy',
+);
+
 const startServer = async () => {
   try {
     await MongoDBConnection.getConnection();
@@ -10,23 +19,11 @@ const startServer = async () => {
       '[DATABASE] Successfully connected to database',
     );
 
-    app.listen(PORT, () => {
-      logger.info(
-        {
-          event: 'server_started',
-          port: PORT,
-          env: process.env.NODE_ENV,
-          version: process.env.npm_package_version,
-        },
-        '[SERVER] Running and healthy',
-      );
-    });
   } catch (err) {
     logger.fatal(
       { event: 'server_start_failed', err },
       '[SERVER] Critical failure while starting',
     );
-    process.exit(1);
   }
 };
 
